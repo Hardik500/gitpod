a0fc6d1e78790398b29a738b0dff10db23bd6f0e
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2022-10-30 06:32:49 +0000

    Minor config changes

--- components/dashboard/config-overrides.js
+++ components/dashboard/config-overrides.js
@@ -1,40 +1,5 @@
 const webpack = require("webpack");
 
-// module.exports = function override(config, env) {
-//     console.log("React app rewired works!");
-//     config.resolve.fallback = {
-//         ...config.resolve.fallback,
-//         crypto: false,
-//         path: false,
-//         os: false,
-//         net: false,
-//         stream: require.resolve("stream-browserify"),
-//         buffer: require.resolve("buffer"),
-//     };
-//     config.plugins = [
-//         ...config.plugins,
-//         new webpack.ProvidePlugin({
-//             process: "process/browser",
-//             Buffer: ["buffer", "Buffer"],
-//         }),
-//     ];
-//     config.devServer = {
-//         ...(config.devServer || {}),
-//         proxy: {
-//             "/api": {
-//                 target: "https://" + process.env.GP_DEV_HOST,
-//                 ws: true,
-//                 headers: {
-//                     host: process.env.GP_DEV_HOST,
-//                     origin: "https://" + process.env.GP_DEV_HOST,
-//                     cookie: process.env.GP_DEV_COOKIE,
-//                 },
-//             },
-//         },
-//     };
-//     return config;
-// };
-
 module.exports = {
     webpack: function (config, env) {
         config.resolve.fallback = {
@@ -59,10 +24,19 @@ module.exports = {
         return function (proxy, allowedHost) {
             // Create the default config by calling configFunction with the proxy/allowedHost parameters
             const config = configFunction(proxy, allowedHost);
-            console.log(config);
-            // Change the dev server's config here...
-            // Then return your modified config.
+
+            config.proxy = {
+                "/api": {
+                    target: "https://" + process.env.GP_DEV_HOST,
+                    ws: true,
+                    headers: {
+                        host: process.env.GP_DEV_HOST,
+                        origin: "https://" + process.env.GP_DEV_HOST,
+                        cookie: process.env.GP_DEV_COOKIE,
+                    },
+                },
+            };
             return config;
         };
-    }
+    },
 };
